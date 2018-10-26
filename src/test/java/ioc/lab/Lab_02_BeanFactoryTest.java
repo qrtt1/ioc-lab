90f9f958ba96a29332c9de9a65a26bd295e4ee3b
Ching Yi, Chan <chingyichan.tw@gmail.com>
2018-10-26 14:12:19 +0800

    fix test rules

--- src/test/java/ioc/lab/Lab_02_BeanFactoryTest.java
+++ src/test/java/ioc/lab/Lab_02_BeanFactoryTest.java
@@ -19,7 +19,10 @@ public class Lab_02_BeanFactoryTest {
         // 驗證是否有抓到 bean
         assertTrue(bf.getBean(FooBar.class) instanceof FooBar);
         assertTrue(bf.getBean(FooBarbar.class) instanceof FooBarbar);
-        assertEquals("富爸爸", bf.getBean(String.class));
+
+//        我們並不會直接把 factory method 的 return type 註冊為 beanName
+//        而是使用它 method name
+//        assertEquals("富爸爸", bf.getBean(String.class));
         assertEquals("富爸爸", bf.getBean("message"));
 
         // 目前的實作會在 IoC Container 內回傳一樣的 instance，也就是 spring 給 bean 的預設 scope: singleton
